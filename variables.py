c681b4bc52acfa92eece255b1ede52864df8858c
CameronA2018 <acameron@sjnma.org>
2018-02-05 10:20:15 -0600

    learned about string variables

---
+++ variables.py
@@ -0,0 +1,7 @@
+first_name = raw_input ("What is your first name, kind Sir?")
+
+last_name = raw_input ("What is your last name, kind Sir?")
+
+full_name = first_name +" "+ last_name
+
+print ("Yes, I suppose that works. The Master will not be excited to see you, so make sure you are polite and don't step on the carpeting, "+full_name +".")
