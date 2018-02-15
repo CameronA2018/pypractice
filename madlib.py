d1195bcaf48888a6024649b798e73a3ab8b02976
CameronA2018 <acameron@sjnma.org>
2018-02-15 10:16:58 -0600

    added madlib

---
+++ madlib.py
@@ -0,0 +1,29 @@
+story = '''As a boy, willy was lonely and sad. He had no friends, was bullied by billy, and tried to commit suicide. One day a little girl told him that she 
+thought he was sad, and that she wanted to help him. Thinking she meant to make fun of him, he pushed her away and created a story in his mind as to why he 
+pushed her away. He told himself that she was an ugly alien from Mars. And soon, he began to believe it. When he saw her in the school hallways, he saw an 
+alien that was trying to eat students, and in genuine fear for his life, he told teachers what he was seeing. They thought he was crazy, and he was sent to 
+a mental institute where he was strapped to a bed in a straight jacket and put in a padded white room. 
+No one outside of the facility ever heard from poor willy again.'''
+name1 = raw_input('Enter a name ')
+verb1 = raw_input('Enter a verb ')
+noun1 = raw_input('Enter a noun ')
+adjective1 = raw_input('Enter an adjective ')
+name2 = raw_input('Enter a second name ')
+verb2 = raw_input('Enter another verb ')
+noun2 = raw_input ('Enter another noun ')
+adjective2 = raw_input('Enter another adjective ')
+noun3 = raw_input('Enter another noun ')
+verb3 = raw_input('Enter another verb ')
+adjective3 = raw_input('Enter another adjective ')
+
+
+adjusted = story.format(name, verb)
+
+print (adjusted)
+
+
+# Madlib Short Story
+##  1111/4  nouns
+##  1111/4  verbs
+##  1111/4  adjectives
+##  11/2  names
---
+++ string.py
@@ -0,0 +1,60 @@
+s=raw_input("Please enter a word: ")
+upper_s = s.upper()
+print (upper_s)
+lower_s = s.lower()
+print (lower_s)
+
+combined = (upper_s + lower_s)
+print (combined)
+
+num_s = len(s)
+print (num_s)
+
+num_combined = len(combined)
+print (num_combined)
+
+halfway = len(s)/2
+print (s[halfway])
+print (s[0])
+print (s[4])
+
+last = len(s)-1
+print (s[last])
+print (s[-1])
+
+firsthalf = s[:halfway]
+lasthalf = s[halfway:len(s)]
+print (lasthalf)
+print (firsthalf)
+
+copy = s[:]
+print (copy)
+
+skip = s[::2]
+print (skip)
+skip2 = s[1::2]
+print (skip2)
+
+value = s[::-1]
+print (value)
+
+print (s.count("l"))
+print (s.find("o"))
+print (s[:s.find("o")])
+
+replaced = s.replace("great","awsome")
+print (replaced)
+
+words = s.split()
+print (words)
+
+numberofwords = len(words)
+print (words[0:2])
+print (words[0:2:-1])
+print (words[::-1])
+
+sentence = "\n".join(words)
+print (sentence)
+
+newsentence = sentence.replace("\n",":-)")
+print (newsentence)
